ea2d987bc9eea64cc6f3e15c8bd77dfb5133234b
Kola Adebayo <kolaadebayo@Kolas-MacBook-Pro.local>
2022-04-10 17:39:55 -0600

    Updates

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -93,7 +93,7 @@ export const currentUser = (
       req.session!.jwt,
       process.env.JWT_KEY!
     ) as UserPayload;
-    req.currentUser = payload;
+    return (req.currentUser = payload);
   } catch (error) {}
   next();
 };
